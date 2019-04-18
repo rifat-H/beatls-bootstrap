6ad6708d22785dfdcd761fb4acae1fe9f7746903
Rifat Hossain <admin@rhossain.me>
2019-04-18 16:33:29 +0600

    another plugin added

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ import "../scss/bundle.scss";
 
 window.$ = window.jQuery = require('jquery');
 require('animatescroll.js');
+require('./plugins/Scrollspy-master/scrollspy.min');
 require('popper.js');
 require('bootstrap');
 require('@fortawesome/fontawesome-free/js/all');
